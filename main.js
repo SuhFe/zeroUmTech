c7b5505643133fa31465637de4319ca5a3a97b09
Suzana <su_zana-santos@hotmail.com>
2025-05-01 20:44:16 -0300

    Clonando objetos

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ const itensInput = document.getElementById("receber-item")
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault() //impedir padrão
-
+    salvarItem()
 })
+
+function salvarItem() {
+    const comprarItem = itensInput.value
+
+    listaDeItens.push({
+        valor: comprarItem
+    })
+
+    console.log(listaDeItens)
+}
